a4cabb22a43ad5af5cb933cff2a54480d53218bb
= <zm275@byu.edu>
2023-11-03 00:30:57 -0600

    game is there
    game is there and work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import SavingsTracker from "./SavingsTracker";
 import Calendar from "./Calendar";
 import "./Calendar.css";
+import ToiletGame from "./ToiletGame";
 
 function App() {
   return (
@@ -13,6 +14,16 @@ function App() {
       </div>
       <div className="calendar-container" id="Calendar">
         <Calendar />
+      </div>
+      <div>
+        {/* Embed the ToiletGame component in an iframe */}
+        <iframe
+          src="/toilet.html" // Replace with the actual path to your toiletgame.html file
+          width="500"
+          height="500"
+          title="Toilet Game"
+          frameBorder="0"
+        />
       </div>
     </div>
   );
